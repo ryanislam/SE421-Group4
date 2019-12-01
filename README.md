e314f2e4ec46a992f5fb973683c0d0e8bee3682d
ryanislam <ryanislam@hotmail.com>
2019-12-01 10:15:27 +0300

    Flask update

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Internet Applications Project Group 4
 
 
 # Eng. Sami Notes:
-1. all images should be moved into `images` folder
-2. image files should renamed into meangingful names (without spaces or !@#$%^&*() chars)
-3. all CSS code should moved into `style.css` file
+1. all images should be moved into `images` folder <!-- Done: Ryan and Ahmed -->
+2. image files should renamed into meangingful names (without spaces or !@#$%^&*() chars) <!-- Done: Ryan and Ahmed>
+3. all CSS code should moved into `style.css` file <!-- Done for Registration, Index: Ryan -->
 4. include menu in all pages
 5. use Bootstrap in Form elements.
 6. add `.gitignore` file
-7. DON'T upload any *.zip file.
+7. DON'T upload any *.zip file. <!-- Zip files removed and integrated into code appropriately: Ryan -->
 
 _sami 28/10/2019_
---
+++ app.py
@@ -0,0 +1,57 @@
+#All code by Ryan Islam
+from flask import Flask, url_for, render_template, request, json
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.secret_key = 'fbd1eefad885bf835e1d5ea884244221'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'ryanislam@hotmail.com'
+app.config['MAIL_PASSWORD'] = 'passwordplaceholder'
+
+mail = Mail(app)
+
+#Error 404 Page
+@app.errorhandler(404)
+def err_404(error):
+    return render_template( '404.html'), 404
+
+#Routing for Index page
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def home():
+    return render_template('index.html', title='Home')
+
+#Routing for Register page
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Registration')
+
+#Routing for About Us Page
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
+
+#Routing for Contact Us Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
+
+#Routing for Data Page
+@app.route('/data')
+def data():
+    return render_template('data.html', title='Data')
+
+
+# print("-- DEBUG MODE ----")
+# app.run(debug = True, port= '5555')
+
+import os
+from waitress import serve
+
+print("--PRODUCTION MODE ---")
+p = os.environ.get('PORT')
+serve(app, host='0.0.0.0', port=p)
