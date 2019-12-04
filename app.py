11ef4ecb042c9b3cc4e92843b46e8a5270cf58f5
ryanislam <ryanislam@hotmail.com>
2019-12-04 07:53:34 +0300

    Paramererized and Email

--- app.py
+++ app.py
@@ -10,27 +10,23 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'ryanislam@hotmail.com'
-app.config['MAIL_PASSWORD'] = 'passwordplaceholder'
+app.config['MAIL_PASSWORD'] = 'ptvKTbXaZ71jt5KunjUo'
 app.config['MAIL_DEFAULT_SENDER'] = "ryanislam@hotmail.com"
 
-#mail = Mail(app)
+mail = Mail(app)
 
-#send email to user and company
-#@app.route('/', methods=['POST'])
-#def send_email():
-#user_email = request.form['user_email']
-#user_message = request.form['user_message']
-
-#send user message to company email
-#usr_msg = Message('Customer - Contact Us', sender= f'{user_email}' , recipients= ['ryanislam@hotmail.com'])
-#usr_msg.body = f'''{user_message}'''
-#mail.send(usr_msg)
-
-#reply confirmation to user
-#msg = Message('We recieved your concerns', sender = 'ryanislam@hotmail.com', recipients = [user_email])
-#msg.body = f''' Thank you { user_email }, we have recieved your email! You will receive a confirmation email from us within the next 2 days'''
-#mail.send(msg)
-#return index()
+# Email Send/Reply/Confirmation
+@app.route('/', methods=['POST'])
+def send_email():
+    user_email = request.form['user_email']
+    user_message = request.form['user_message']
+    usr_msg = Message('Customer - Contact Us', sender= f'{user_email}' , recipients= ['ryanislam@hotmail.com'])
+    usr_msg.body = f'''{user_message}'''
+    mail.send(usr_msg)
+    msg = Message('We recieved your concerns', sender = 'ryanislam@hotmail.com', recipients = [user_email])
+    msg.body = f''' Thank you { user_email }, we have recieved your email! You will receive a confirmation email from us within the next 2 days'''
+    mail.send(msg)
+    return index()
 
 # Error 404 Page
 @app.errorhandler(404)
@@ -43,6 +39,11 @@ def err_404(error):
 @app.route('/home')
 def home():
     return render_template('index.html', title='Home')
+
+# Parameterized Request
+@app.route('/hotel_list/<name>/<path:img>')
+def hotelviewingalt(name,img):
+    return render_template('hotel_viewing.html',name=name,img=img)
 
 # Routing for Register page
 @app.route('/register')
@@ -64,11 +65,21 @@ def contact():
 def data():
     return render_template('data.html', title='Data')
 
+# Routing for Hotel Viewing
+@app.route('/hotel_list')
+def hotelviewing():
+    return render_template('hotel_list.html', title='Viewing')
 
-# print("-- DEBUG MODE ----")
-# app.run(debug = True, port= '5555')
+# Parameterized Request
+@app.route('/hotel_list/<name>')
+def hotelviewingsecond(name):
+    return render_template('hotel_viewing.html',name=name)
 
-# Waitress 
+
+#print("-- DEBUG MODE ----")
+#app.run(debug = True, port= '5555')
+
+Waitress 
 import os
 from waitress import serve
 
